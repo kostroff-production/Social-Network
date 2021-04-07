8c0d4ae0a439628cc8f573edb7787ea5931d84e0
Kostrov-Producsion <44669971+Kostrov-Producsion@users.noreply.github.com>
2021-04-07 19:27:46 +0300

    Update Audio.js

--- static/Audio.js
+++ static/Audio.js
@@ -144,7 +144,7 @@ function loadAudio () {
 
 // функция загрузки треков после прогрузки плейлиста
 function load_track(index_no){
-	if (All_song[index_no].audio) {
+	if (All_song.length > 0) {
 		clearInterval(timer); // сбрасываем временной интервал прошлой песни
 		reset_slider(); // сбрасываем бегунок продолжительности песни
 
