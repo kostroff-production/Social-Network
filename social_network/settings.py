5f097e1ada213c91773e1d4b89b7d67fd91ddc2a
Kostrov-Producsion <44669971+Kostrov-Producsion@users.noreply.github.com>
2021-04-10 16:09:51 +0300

    Update settings.py

--- social_network/settings.py
+++ social_network/settings.py
@@ -76,7 +76,7 @@ LOGGING = {
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%4dyz@lhes*^r0%_%#2_oq1wk2xz=lu0wen623p%8qlv*kzq2='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -144,18 +144,18 @@ AUTHENTICATION_BACKENDS = [ # указываем на нашу кастомну�
 ]
 
 ADMINS = [
-    ('kostroff.jenya@yandex.ru', 'kostroff.jenya@yandex.ru')
+    ('', '')
 ]
 
 EMAIL_SUBJECT_PREFIX = '[Django] '
-EMAIL_HOST = 'fornex.com'
-EMAIL_HOST_USER = 'kostroff.jenya@yandex.ru'
-EMAIL_HOST_PASSWORD = 'aH5PleGZSP7XW9sw'
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
-SERVER_EMAIL = 'kostroff.jenya@yandex.ru'
-DEFAULT_FROM_EMAIL = 'kostroff.jenya@yandex.ru'
+SERVER_EMAIL = ''
+DEFAULT_FROM_EMAIL = ''
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
@@ -163,11 +163,11 @@ DEFAULT_FROM_EMAIL = 'kostroff.jenya@yandex.ru'
 DATABASES = { # прописываем от куда подключать БД
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Social-Network',
-        'USER': 'Jony',
-        'PASSWORD': 'Jony8889',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
